01a774de94085276b47872ce0d07b288cdd5b8f6
Phong Hoang <p.hoang6900@gmail.com>
2023-04-28 22:42:41 -0400

    Added proxy

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 
-export const BASE_URL = 'https://anglers-atlas-backend.herokuapp.com/api'
+export const BASE_URL = 'https://anglers-atlas-backend.herokuapp.com'
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
