6e00921794b04bd7cefa615b564b240f9df99a93
Phong Hoang <p.hoang6900@gmail.com>
2023-04-28 21:57:28 -0400

    Changed baseURL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 
-export const BASE_URL = 'https://anglers-atlas-backend.herokuapp.com'
+export const BASE_URL = 'https://anglers-atlas-backend.herokuapp.com/api'
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
